c534ce012eeefc42793477ebac851412f7feaaa8
Dang Quang Thai <drthai1101@gmail.com>
2026-07-20 11:00:44 +0700

    BTVN buoi 2

--- BTVN buoi 2/src/bai2.java
+++ BTVN buoi 2/src/bai2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TriangleClassifier {
+public class bai2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
--- BTVN buoi 2/src/bai3.java
+++ BTVN buoi 2/src/bai3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PasswordValidator {
+public class bai3 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
